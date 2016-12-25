54914106402c5cef6d2d7a7f6b429cc391b96463
Justin Peck <jdpeck90@gmail.com>
2016-12-24 23:24:19 -0500

    Add Lesson 4 & 5

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,11 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom'; //<-- only render component of package
+// import ReactDom from 'react-dom'; <--Full package
+import './css/style.css's
+
+import StorePicker from './components/StorePicker';
+
+
+
+render (<StorePicker/>, document.querySelector('#main'));
---
+++ catch-of-the-day/src/storepicker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class StorePicker extends React.Compenent {
+  render() {
+    return (
+        <form className="store-select">
+            { /* This is how you write a comment */}
+            <h2>please emter -------TEST---------- placeholder="Store Name" />
+            <button type="submit">  Click Here!</button>
+        </form>
+      )
+  }
+}
