d058e73560493fa90d9176e94f6cebdbc091cace
Justin Peck <jdpeck90@gmail.com>
2016-12-26 23:27:04 -0500

    Lesson 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDgugBQTYwUuJwJV1O2697TFNZ99bvD9gE",
+    authDomain: "catch-of-the-day-jp-a51e5.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-jp-a51e5.firebaseio.com",
+});
+
+export default base;
