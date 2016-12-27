3b7dda13fd1acc78f46fcc56a1ef59258772e405
Justin Peck <jdpeck90@gmail.com>
2016-12-26 23:00:25 -0500

    Finished Lesson 17

--- catch-of-the-day/src/components/untitled.js
+++ catch-of-the-day/src/components/untitled.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { formatPrice } from '../helpers';
+import {formatPrice} from '../helpers'
 class Order extends React.Component {
-  constructor() {
+   constructor() {
     super();
     this.renderOrder = this.renderOrder.bind(this);
   }
@@ -11,44 +11,42 @@ renderOrder(key) {
   const count = this.props.order[key];
 
   if(!fish || fish.status === 'unavailable') {
-    return <li key={key}>Sorry, {fish ? fish.name : 'fish'} is no longer available!</li>
+    return <li key={key}>Sorry, {fish ? fish.name : 'fish'} is no longer available</li>
   }
-
   return (
-    <li key={key}>
-    <span>{count}lbs {fish.name} </span>
-    <span className="price">{formatPrice(count * fish.price)}</span>
-    </li>
+      <li key={key}>
+      <span>{count}lbs {fish.name}</span>
+      <span className="price">{formatPrice(count * fish.price)}</span>
     )
-
 }
 
-  render() {
-  const orderIds = Object.keys(this.props.order);
-    const total = orderIds.reduce((prevTotal, key) => {
-      const fish = this.props.fishes[key];
+render() {
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce(prevTotal, key) => {
+      const fish =  this.props.fishes[key];
       const count = this.props.order[key];
-      const isAvailable = fish && fish.status === ' available';
-      console.log(orderIds)
+      const isAvailable = fish && fish.status === 'available';
       if(isAvailable) {
         return prevTotal + (count * fish.price || 0)
       }
       return prevTotal;
-    }, 0)
+      }, 0);
       return (
       <div className="order-wrap">
         <h2>Your Order</h2>
         <ul className="order">
-        {orderIds.map(this.renderOrder)}
+        {{orderIds.map(this.renderOrder}}
           <li className="total">
             <strong>Total:</strong>
-              {total}
+            {formatPrice(total)}
           </li>
         </ul>
-
-
       </div>
       )
+
+
+
+
   }
 }
 
